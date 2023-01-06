3862c753233ca75bb5a1bd756c469f7a292123c7
William Chow <williamchow604@gmail.com>
2023-01-05 21:16:38 -0800

    Fixed background image bug

--- script.js
+++
@@ -1,8 +0,0 @@
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-};
-
-ready(() => {
-    document.querySelector(".bgimg").style.height = window.innerHeight + "px";
-});
