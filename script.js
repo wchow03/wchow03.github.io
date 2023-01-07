49827b170c2eb8e816dbc58b7880c626e2c3fb92
William Chow <williamchow604@gmail.com>
2023-01-07 01:29:03 -0800

    Added animated progress bar for skills/languages

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const navbar = document.querySelector(".navbar");
+const skills = document.querySelector(".progress");
 
 window.onscroll = function() {
     let top = window.scrollY;
-    if (top >= 900) {
+    if (top >= 650) {
         navbar.classList.add("bg-dark");
     } else {
         navbar.classList.remove("bg-dark");
     }
 };
+
+
+
