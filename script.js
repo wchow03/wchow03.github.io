bb1d7a22b3b9dc21a59c19b1a46bc91eefdb39c8
William Chow <williamchow604@gmail.com>
2023-01-07 22:52:07 -0800

    Added a reveal on scroll animation

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const navbar = document.querySelector(".navbar");
-const skills = document.querySelector(".progress");
 
 window.onscroll = function() {
     let top = window.scrollY;
@@ -10,5 +9,41 @@ window.onscroll = function() {
     }
 };
 
+// Using an Intersection Observer to reveal elements on scroll
+
+const hiddenElems = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+            entry.target.classList.remove("hidden");
+        }
+    })
+},
+{
+    threshold: 0.6,
+});
+
+hiddenElems.forEach(elem => {
+    observer.observe(elem);
+})
 
 
+const skills = document.querySelectorAll(".progress");
+const skillContainer = document.querySelector(".skill-container");
+
+const skillObserver = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                skills.forEach(skill => {
+                    skill.classList.add("active");
+                });
+            }
+        })
+    },
+    {
+        threshold: 1
+    });
+
+skillObserver.observe(skillContainer);
